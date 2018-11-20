84402dfd1677a29cc6ee7f58d3c7398315800e73
unknown <amm-andreistrizhko@yandex.ru>
2018-11-20 11:18:22 +0300

    del default components. It's begin

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <div class="nav-bar">NAVIGATION MENU</div>
+    <div>
+      <router-view/>
+    </div>
   </div>
 </template>
 
@@ -11,13 +13,19 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped lang="scss">
+.nav-bar {
+  width: 100%;
+  height: 60px;
+  z-index: 100;
+  background-color: blue;
+  margin: 0;
+  padding: 10px;
+}
+</style>
+
+<style lang="scss">
+body{
+  margin: 0;
 }
 </style>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="main-block">
+  <h3>Бобро дожаловать</h3>
+  <div class="discription">
+    <p>длинное и не нужное описание сайта</p>
+  </div>
+  <div>
+    <button class="button big-button"> BIG BUTTON перейти к заказу документов</button>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+@import "../assets/css/main.scss";
+.big-button {
+  width: 400px;
+  height: 200px;
+}
+</style>
