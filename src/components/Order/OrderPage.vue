2f1fefa266300b27fc7e61d4da90f0120a6eb380
Andre <amm-andreistrizhko@yandex.ru>
2018-12-16 13:59:52 +0300

    add field

--- src/components/Order/OrderPage.vue
+++ src/components/Order/OrderPage.vue
@@ -4,24 +4,27 @@
     <div class="order-main-block">
       <div class="inputs-block">
         <div class="form-group fix-form-group">
-          <input class="input" v-bind="userSecondName" type="text" id="dynamic-label-input" placeholder="Фамилия">
+          <input class="input" v-model="userSecondName" type="text" id="dynamic-label-input" placeholder="Фамилия">
           <label for="dynamic-label-input">Фамилия</label>
         </div>
       </div>
       <div class="inputs-block">
         <div class="form-group fix-form-group">
-          <input class="input" v-bind="userFirstName" type="text" id="dynamic-label-input" placeholder="Имя">
+          <input class="input" v-model="userFirstName" type="text" id="dynamic-label-input" placeholder="Имя">
           <label for="dynamic-label-input">Имя</label>
         </div>
       </div>
       <div class="inputs-block">
         <div class="form-group fix-form-group">
-          <input class="input" v-bind="userLastName" type="text" id="dynamic-label-input" placeholder="Отчество">
+          <input class="input" v-model="userLastName" type="text" id="dynamic-label-input" placeholder="Отчество">
           <label for="dynamic-label-input">Отчество</label>
         </div>
       </div>
       <div class="inputs-block">
-        in
+        <div class="input category-select" @click="showCategory()">
+          <p>{{ selectedCategory || 'выберите категорию' }}</p>
+        </div>
+        <div class="input-select-item" v-if="showCategoryFlag" @click="selectCategory(item)" v-for="item in categories" v-bind:key="item">{{ item }}</div>
       </div>
       <button class="button order-button" @click="goNext()">Далее</button>
     </div>
@@ -30,19 +33,47 @@
 
 <script>
 import orderStore from './orderStore'
+import customInput from './OrderUtils/CustomInput'
 export default {
+  name: 'Order',
+  components: {'customInput': customInput},
   data () {
     return {
       userFirstName: '',
       userLastName: '',
-      userSecondName: ''
+      userSecondName: '',
+      categories: [
+        'Ралли', 'АКГ серийный', 'АКГ формула', 'Картинг',
+        'Рекордный', 'Кросс Див 1,2', 'Кросс СКА',
+        'Кросс полноприводный', 'Грузовой', 'Другое'
+      ],
+      showCategoryFlag: false,
+      selectedCategory: null
     }
   },
   computed: {
   },
   methods: {
     goNext () {
-
+      if (this.checkInput()) {
+        orderStore.state.firstName = this.userFirstName
+        orderStore.state.secondName = this.userSecondName
+        orderStore.state.lastName = this.userLastName
+        orderStore.state.category = this.selectedCategory
+      }
+    },
+    checkInput () {
+      return true
+    },
+    setValue (val) {
+      console.log(val)
+    },
+    selectCategory (value) {
+      this.selectedCategory = value
+      this.showCategoryFlag = false
+    },
+    showCategory () {
+      this.showCategoryFlag = !this.showCategoryFlag
     }
   }
 }
@@ -59,5 +90,20 @@ export default {
 }
 .fix-form-group {
   padding-left: 10px;
+}
+.category-select {
+  cursor: default;
+  padding: 5px 10px 0 10px;
+  border-bottom: 1px solid rgb(212, 212, 212);
+  color: gray;
+  margin-bottom: 20px;
+}
+.input-select-item {
+  margin-left: 10px;
+  padding: 5px;
+  border-radius: 3px;
+}
+.input-select-item:hover {
+  background-color: rgba(128, 128, 128, 0.658);
 }
 </style>
---
+++ src/components/Order/OrderUtils/CustomInput.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="main-block">
+    <div class="form-group fix-form-group">
+      <input class="input" v-on:input="sendValue" v-model="inputValue" type="text" id="dynamic-label-input" v-bind:placeholder="lableText" >
+      <label for="dynamic-label-input">{{  lableText }}</label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'customInput',
+  props: ['lableText'],
+  data () {
+    return {
+      inputValue: ''
+    }
+  },
+  methods: {
+    sendValue () {
+      this.$emit('inputValue', this.inputValue)
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.input {
+  outline: none;
+  width: 100%;
+  padding: 5px 10px;
+  border-top: 0;
+  border-left: 0;
+  border-right: 0;
+  font-size: 12pt;
+}
+
+.form-group {
+  position: relative;
+  padding-top: 1.5rem;
+  margin: 30px 0;
+}
+
+label {
+  position: absolute;
+  top: 0;
+  left: 18px;
+  font-size: 10pt;
+  opacity: 1;
+  transform: translateY(0);
+  transition: all 0.2s ease-out;
+}
+
+input:placeholder-shown + label {
+  opacity: 0;
+  transform: translateY(1rem);
+}
+</style>
